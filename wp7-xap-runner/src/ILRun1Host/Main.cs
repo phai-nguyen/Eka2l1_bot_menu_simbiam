f872c019341e6f7db8ae2af5475f7cd0dcec8e0a
phai-nguyen <142614224+phai-nguyen@users.noreply.github.com>
2026-09-26 18:40:58 +0700

    wp7: implement ILRUN1 dynamic assembly execution

---
+++ wp7-xap-runner/src/ILRun1Host/Main.cs
@@ -0,0 +1,11 @@
+using UIKit;
+
+namespace WP7ILRun1;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+}
