7b5967c62a64b2380be995c2b86277313834a0bd
phai-nguyen <142614224+phai-nguyen@users.noreply.github.com>
2026-09-26 18:45:14 +0700

    wp7: align ILRUN1 AppDelegate nullability

--- wp7-xap-runner/src/ILRun1Host/AppDelegate.cs
+++ wp7-xap-runner/src/ILRun1Host/AppDelegate.cs
@@ -8,7 +8,7 @@ public sealed class AppDelegate : UIApplicationDelegate
 {
     public override UIWindow? Window { get; set; }
 
-    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+    public override bool FinishedLaunching(UIApplication application, NSDictionary? launchOptions)
     {
         Window = new UIWindow(UIScreen.MainScreen.Bounds)
         {
