6a82bd13f54866c2434975c97d0b4a53ce842531
phai-nguyen <142614224+phai-nguyen@users.noreply.github.com>
2026-09-26 18:41:00 +0700

    wp7: implement ILRUN1 dynamic assembly execution

---
+++ wp7-xap-runner/src/ILRun1Host/AppDelegate.cs
@@ -0,0 +1,20 @@
+using Foundation;
+using UIKit;
+
+namespace WP7ILRun1;
+
+[Register("AppDelegate")]
+public sealed class AppDelegate : UIApplicationDelegate
+{
+    public override UIWindow? Window { get; set; }
+
+    public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+    {
+        Window = new UIWindow(UIScreen.MainScreen.Bounds)
+        {
+            RootViewController = new MainViewController()
+        };
+        Window.MakeKeyAndVisible();
+        return true;
+    }
+}
